b623a10a9b3beff72f6ffc2ad890008128a2dba9
Akshay Chugh <akshaychugh995@gmail.com>
2024-11-06 16:09:06 +0530

    added vercel analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,6 +9,7 @@ import './api/axiosSetup';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
